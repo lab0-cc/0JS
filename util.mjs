cf873becde7a851fdf0cd41cea59a9145da080e1
Benjamin Somers <benjamin.somers@resel.fr>
2025-08-14 23:44:59 +0200

    Add attribute name sluggification
    Signed-off-by: Benjamin Somers <benjamin.somers@resel.fr>

--- util.mjs
+++ util.mjs
@@ -6,7 +6,7 @@ export function createElement(tag, className=null, attributes={}, content=null) 
     if (className !== null)
         el.className = className;
     for (const attribute in attributes)
-        el.setAttribute(attribute, attributes[attribute]);
+        el.setAttribute(attribute.replace(/_$/, '').replace(/([A-Z])/g, '-$1').toLowerCase(), attributes[attribute]);
     if (content !== null)
         el.textContent = content;
     return el;
